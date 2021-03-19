869292c9e1a29520eca92d3e2760cf646d0dca05
jechoiiii <jechoiiii@gmail.com>
2021-03-19 19:56:56 +0900

    first commit

---
+++ WalkOrder/src/main/java/com/wo/order/WalkOrderApplication.java
@@ -0,0 +1,13 @@
+package com.wo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkOrderApplication.class, args);
+	}
+
+}
---
+++ WalkOrder/src/test/java/com/wo/order/WalkOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
